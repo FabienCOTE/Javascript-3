001b21a5e79bac437d56d6383c68260c2dfcaaca
FabienCOTE <fabien.cote@gmail.com>
2018-10-11 12:23:23 +0200

    Fin exercice

--- Exercice_2/assets/js/javascript.js
+++ Exercice_2/assets/js/javascript.js
@@ -1,8 +1,12 @@
 //Création de deux variables avec querySelectorAll qui permet de selectionner tous les éléments
 //On selectionne le premier élément [0]
 var x = document.querySelectorAll("[href='#']")[0];
+
 //On selectionne le second élément [1]
 var y = document.querySelectorAll("[href='#']")[1];
+
+//Création de la variable text
+var text = document.getElementById("text");
 
 //Création d'un événement sur le bouton "afficher" et "masquer"
 //On applique la fonction show et hide
@@ -11,10 +15,10 @@ y.addEventListener("click", hide);
 
 //Fonction show
 function show() {
-  document.getElementById("text").style.display = "block";
+  text.style.display = "block";
 }
 
 //Fonction hide
 function hide() {
-  document.getElementById("text").style.display = "none";
+  text.style.display = "none";
 }
--- Exercice_3/assets/js/javascript.js
+++ Exercice_3/assets/js/javascript.js
@@ -1,7 +1,8 @@
-//Création de trois variables pour green, red et blue
+//Création de 4 variables
 var g = document.getElementsByClassName("green");
 var r = document.getElementsByClassName("red");
 var b = document.getElementsByClassName("blue");
+var text = document.getElementById("text");
 
 //On selectionne le premier élément de la class et on lui applique une fonction
 g[0].addEventListener("click", green);
@@ -10,15 +11,15 @@ b[0].addEventListener("click", blue);
 
 //Fonction green
 function green() {
-  document.getElementById("text").style.color = "green";
+  text.style.color = "green";
 }
 
 //Fonction red
 function red() {
-  document.getElementById("text").style.color = "red";
+  text.style.color = "red";
 }
 
 //Fonction blue
 function blue() {
-  document.getElementById("text").style.color = "blue";
+  text.style.color = "blue";
 }
